b60c5dd6eea208a82e2134d327a000ce4dd36233
Yourone1 <snoof95@mail.ru>
2019-11-11 20:04:42 +0300

    New

---
+++ portalapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=140 )
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
